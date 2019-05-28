5119067f3fca6b06a6dda749bdd0a25dc03d26e8
CristobalSalazar <chris.salza@gmail.com>
2019-05-27 23:06:46 -0400

    added control of profiles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
 import Sidebar from "./components/sidebar";
 import Newsfeed from "./components/newsfeed";
 import Dashboard from "./components/dashboard";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="master">
-        <Sidebar />
-        <div className="container">
-          <Dashboard companyId="aapl" />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.setProfile.bind(this);
+  }
+  state = { profile: "aapl" };
+
+  setProfile = profile => {
+    console.log(profile);
+    this.refs.dashboard.onProfileChange(profile);
+    this.setState({ profile });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="master">
+          <Sidebar onClick={this.setProfile} />
+          <div className="container">
+            <Dashboard ref="dashboard" profile={this.state.profile} />
+          </div>
+          <Newsfeed />
         </div>
-        <Newsfeed />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
